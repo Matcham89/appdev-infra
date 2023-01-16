67d2b57d911b49965825d665c86dcd4e756cfa83
Matcham89 <cjmatcham@hotmail.co.uk>
2023-01-16 19:15:55 +0000

    updates to remote state

--- iac/main.tf
+++ iac/main.tf
@@ -132,19 +132,19 @@ resource "google_service_account_iam_member" "actas_cloud_scheduler_sa" {
 ############# Cloud Scheduler YouGov ###############
 ####################################################
 
-data "google_cloud_run_service" "cr_yougov" {
+data "google_cloud_run_service" "cr_data" {
   project  = var.project_id
   location = local.default_region
-  name     = "cr-mcm-createlift-yougov-data"
+  name     = "cr-hello-world"
 
 }
 
 
-resource "google_cloud_scheduler_job" "cs_yougov" {
+resource "google_cloud_scheduler_job" "cs" {
   project     = var.project_id
   region      = local.default_region
-  name        = "yougov-data-collection-${random_id.suffix.hex}"
-  description = "Collect data from the yougov source"
+  name        = "data-collection-${random_id.suffix.hex}"
+  description = "Collect data from source"
   schedule    = "30 7 14,28 * *"
   time_zone   = "Europe/London"
   depends_on = [
@@ -162,47 +162,7 @@ resource "google_cloud_scheduler_job" "cs_yougov" {
 
   http_target {
     http_method = "GET"
-    uri         = "${data.google_cloud_run_service.cr_yougov.status[0].url}/api/trigger/schedule"
-    oidc_token {
-      service_account_email = google_service_account.sa_scheduler.email
-    }
-  }
-}
-
-
-####################################################
-############# Cloud Scheduler Nielsen ##############
-####################################################
-data "google_cloud_run_service" "cr_nielsen" {
-  project  = var.project_id
-  location = local.default_region
-  name     = "cr-mcm-createlift-nielsen-data"
-
-}
-
-resource "google_cloud_scheduler_job" "cs_nielsen" {
-  project     = var.project_id
-  region      = local.default_region
-  name        = "nielsen-data-collection"
-  description = "Collect data from the nielsen source"
-  schedule    = "30 7 14,28 * *"
-  time_zone   = "Europe/London"
-  depends_on = [
-    google_service_account.sa_scheduler
-  ]
-
-  retry_config {
-    min_backoff_duration = "5s"
-    max_backoff_duration = "3600s"
-    max_retry_duration   = "10s"
-    max_doublings        = 2
-    retry_count          = 3
-  }
-
-
-  http_target {
-    http_method = "GET"
-    uri         = "${data.google_cloud_run_service.cr_nielsen.status[0].url}/api/trigger/schedule"
+    uri         = "${data.google_cloud_run_service.cr_data.status[0].url}/api/trigger/schedule"
     oidc_token {
       service_account_email = google_service_account.sa_scheduler.email
     }
@@ -218,9 +178,9 @@ resource "random_id" "suffix" {
   byte_length = 3
 }
 
-resource "google_cloud_tasks_queue" "yougov_queue" {
+resource "google_cloud_tasks_queue" "queue" {
   project  = var.project_id
-  name     = "yougov-queue-${random_id.suffix.hex}"
+  name     = "queue-${random_id.suffix.hex}"
   location = local.default_region
 
   rate_limits {
@@ -240,33 +200,6 @@ resource "google_cloud_tasks_queue" "yougov_queue" {
   }
 }
 
-
-
-####################################################
-############### Cloud Tasks Nielsen ################
-####################################################
-
-resource "google_cloud_tasks_queue" "nielsen_queue" {
-  project  = var.project_id
-  name     = "nielsen-queue-${random_id.suffix.hex}"
-  location = local.default_region
-
-  rate_limits {
-    max_concurrent_dispatches = 3
-    max_dispatches_per_second = 1
-  }
-
-  retry_config {
-    max_attempts       = 10
-    max_retry_duration = "4s"
-    max_backoff        = "3600s"
-    min_backoff        = "2s"
-    max_doublings      = 8
-  }
-  lifecycle {
-    create_before_destroy = true
-  }
-}
 
 
 
@@ -275,53 +208,33 @@ resource "google_cloud_tasks_queue" "nielsen_queue" {
 ###################################################
 
 
-resource "google_storage_bucket" "yougov_gcs" {
+resource "google_storage_bucket" "gcs" {
   project                     = var.project_id
-  name                        = "bkt-${var.project_id}-yougov-gcs"
+  name                        = "bkt-${var.project_id}-gcs"
   location                    = local.default_region
   uniform_bucket_level_access = true
   force_destroy               = true
 }
 
-resource "google_storage_bucket_iam_member" "yougov_gcs_member" {
-  bucket = google_storage_bucket.yougov_gcs.name
+resource "google_storage_bucket_iam_member" "gcs_member" {
+  bucket = google_storage_bucket.gcs.name
   role   = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.sa_cr_yougov.email}"
-}
-
-
-###################################################
-################### Nielsen GCS  ##################
-###################################################
-
-
-resource "google_storage_bucket" "nielsen_gcs" {
-  project                     = var.project_id
-  name                        = "bkt-${var.project_id}-nielsen-gcs"
-  location                    = local.default_region
-  uniform_bucket_level_access = true
-  force_destroy               = true
-}
-
-resource "google_storage_bucket_iam_member" "nielsen_gcs_member" {
-  bucket = google_storage_bucket.nielsen_gcs.name
-  role   = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.sa_cr_nielsen.email}"
+  member = "serviceAccount:${google_service_account.sa_cr.email}"
 }
 
 
 
 ###################################################
-############### YouGov Big Query  #################
+############### Big Query  #################
 ###################################################
 
 
 
-resource "google_bigquery_dataset" "yougov_dataset" {
+resource "google_bigquery_dataset" "dataset" {
   project       = var.project_id
-  dataset_id    = "yougov_raw"
-  friendly_name = "yougov-raw"
-  description   = "yougov data set"
+  dataset_id    = "raw"
+  friendly_name = "aw"
+  description   = "data set"
   location      = "EU"
 
 
@@ -332,8 +245,8 @@ resource "google_bigquery_dataset" "yougov_dataset" {
 
 resource "google_bigquery_table" "CSV" {
   project    = var.project_id
-  dataset_id = google_bigquery_dataset.yougov_dataset.dataset_id
-  table_id   = "yougov-raw-sheet"
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = "raw-sheet"
 
 
 
@@ -449,10 +362,10 @@ locals {
 }
 
 
-resource "google_bigquery_table" "yougov_query_table" {
+resource "google_bigquery_table" "query_table" {
   project    = var.project_id
-  dataset_id = google_bigquery_dataset.yougov_dataset.dataset_id
-  table_id   = "yougov-data-view"
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = "data-view"
 
 
   view {
@@ -463,7 +376,7 @@ resource "google_bigquery_table" "yougov_query_table" {
     SELECT 
       *,
       rank() over (partition by tbl.date, tbl.region, tbl.sector_id, tbl.brand_id, tbl.metric, tbl.query order by tbl.ingest_datetime desc) as rank
-    FROM `${local.project}.yougov_raw.yougov-raw-sheet` as tbl
+    FROM `${local.project}.raw.raw-sheet` as tbl
   ) 
   WHERE 
     rank = 1 and score != ""
@@ -472,7 +385,7 @@ resource "google_bigquery_table" "yougov_query_table" {
 
   deletion_protection = false # important
   depends_on = [
-    google_bigquery_dataset.yougov_dataset
+    google_bigquery_dataset.dataset
   ]
 }
 
@@ -496,11 +409,11 @@ resource "google_secret_manager_secret" "secret-basic" {
 
 
 ###################################################
-############ yougov_username Secret  ##############
+############ username Secret  ##############
 ###################################################
-resource "google_secret_manager_secret" "yougov_username" {
+resource "google_secret_manager_secret" "username" {
   project   = var.project_id
-  secret_id = "sms-createlift-yougov-un-${var.project_id}"
+  secret_id = "sms-un-${var.project_id}"
 
   replication {
     user_managed {
@@ -514,115 +427,11 @@ resource "google_secret_manager_secret" "yougov_username" {
 
 
 ###################################################
-############ yougov_password Secret  ##############
+############ password Secret  ##############
 ###################################################
-resource "google_secret_manager_secret" "yougov_password" {
+resource "google_secret_manager_secret" "password" {
   project   = var.project_id
-  secret_id = "sms-createlift-yougov-pw-${var.project_id}"
-
-  replication {
-    user_managed {
-      replicas {
-        location = local.default_region
-      }
-    }
-  }
-}
-
-
-###################################################
-######## monogo db connection str secret  #########
-###################################################
-resource "google_secret_manager_secret" "nielsen_mongo_connection_str" {
-  project   = var.project_id
-  secret_id = "sms-createlift-nielsen-mdb-conn-str-${var.project_id}"
-
-  replication {
-    user_managed {
-      replicas {
-        location = local.default_region
-      }
-    }
-  }
-}
-
-
-
-###################################################
-############# okta client ID secret  ##############
-###################################################
-resource "google_secret_manager_secret" "webapp_okta_clientid" {
-  project   = var.project_id
-  secret_id = "sms-createlift-webapp-okta-clientid-${var.project_id}"
-
-  replication {
-    user_managed {
-      replicas {
-        location = local.default_region
-      }
-    }
-  }
-}
-
-
-###################################################
-########### webapp nextauth secret  ##############
-###################################################
-resource "google_secret_manager_secret" "webapp_nextauth_secret" {
-  project   = var.project_id
-  secret_id = "sms-createlift-webapp-nextauth-secret-${var.project_id}"
-
-  replication {
-    user_managed {
-      replicas {
-        location = local.default_region
-      }
-    }
-  }
-}
-
-
-
-###################################################
-############## okta client secret  ##############
-###################################################
-resource "google_secret_manager_secret" "webapp_okta_clientsecret" {
-  project   = var.project_id
-  secret_id = "sms-createlift-webapp-okta-clientsecret-${var.project_id}"
-
-  replication {
-    user_managed {
-      replicas {
-        location = local.default_region
-      }
-    }
-  }
-}
-
-
-###################################################
-############### okta client issuer  ###############
-###################################################
-resource "google_secret_manager_secret" "webapp_okta_issuer" {
-  project   = var.project_id
-  secret_id = "sms-createlift-webapp-okta-issuer-${var.project_id}"
-
-  replication {
-    user_managed {
-      replicas {
-        location = local.default_region
-      }
-    }
-  }
-}
-
-
-###################################################
-############  webapp database url #################
-###################################################
-resource "google_secret_manager_secret" "webapp_database_url" {
-  project   = var.project_id
-  secret_id = "sms-createlift-webapp-database-url-${var.project_id}"
+  secret_id = "sms-pw-${var.project_id}"
 
   replication {
     user_managed {
--- iac/remote_states.tf
+++ iac/remote_states.tf
@@ -9,4 +9,5 @@ data "terraform_remote_state" "bootstrap" {
 locals {
   default_region   = data.terraform_remote_state.bootstrap.outputs.default_region
   artifact_repo_id = data.terraform_remote_state.bootstrap.outputs.google_artifact_registry_repository_name
+  cicd_project_id  = data.terraform_remote_state.bootstrap.outputs.cicd_project_id
 }
