084b6df9e84adaf957b80a0275fb22d51b8b335b
Matcham89-AB <chris.matcham@appsbroker.com>
2023-01-19 13:39:38 +0000

    clean slate

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "bkt-my-project-35513-cicd"
+    bucket = ""
     prefix = "bootstrap"
   }
 }
--- iac/remote_states.tf
+++ iac/remote_states.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "bootstrap" {
   backend = "gcs"
   config = {
-    bucket = "bkt-my-project-35513-cicd"
+    bucket = ""
     prefix = "bootstrap"
   }
 }
